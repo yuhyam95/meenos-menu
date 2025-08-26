461cf7d3bc38058b1f7a3187065b22e62be8c02b
Yusuf Habu <yuhyam95@gmail.com>
2025-08-26 16:29:11 +0000

    make the food cards more mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         </TabsList>
       </Tabs>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {filteredItems.map((item) => (
           <FoodItemCard key={item.id} item={item} />
         ))}

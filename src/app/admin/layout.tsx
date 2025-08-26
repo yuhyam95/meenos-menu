ff74d9cbd5b913a131cde2b1b46dfcf46c28b3b9
Yusuf Habu <yuhyam95@gmail.com>
2025-08-26 16:37:32 +0000

    The header is coverning the menu items

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -26,7 +26,7 @@ export default function AdminLayout({
       <SidebarProvider>
         <div className="flex min-h-screen w-full flex-col">
           <Header/>
-          <div className="flex flex-1">
+          <div className="flex flex-1 pt-16">
             <Sidebar>
                 <SidebarHeader>
                   <div className="flex items-center gap-2">
